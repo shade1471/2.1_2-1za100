c93f54dfad193c70216f0029f58588b277da3314
Andrey Gribanov <shade1471@gmail.com>
2021-11-27 16:08:34 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        // Начальное значение счета
+        int account = 100;
+        // Сумма пополнения
+        int refill = 1000;
+
+
+        boolean bonusYes = refill > 1000;
+        int totalAccount = account + refill;
+        int bonus = refill / 100;
+        if (bonusYes) {
+            totalAccount = totalAccount + bonus;
+            System.out.println("Вы получаете от нас бонус " + bonus + " руб.");
+            System.out.println("Сумма средств на вашем счете c учетом полученных бонусов: " + totalAccount + " руб.");
+        } else {
+            System.out.println("Сумма средств на вашем счете: " + totalAccount + " руб. Бонусных рублей не начислено");
+        }
+    }
+    // Для практики с тернарным оператором
+
+//        boolean bonusYes = refill > 1000;
+//        int bonus = refill / 100;
+//        int totalAccount = account + refill;
+//        totalAccount = bonusYes ? totalAccount + bonus : totalAccount;
+//        if (bonusYes) {
+//            System.out.println("Вы получаете от нас бонус " + bonus + " руб.");
+//        }
+//        System.out.println("Сумма средств на вашем счете: " + totalAccount + " руб.");
+//  }
+}
